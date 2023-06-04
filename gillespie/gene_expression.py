dd675a7b812cdde4a912ddf24fe6cbe3a070b7ce
JackieJCZ <122524849+JackieJCZ@users.noreply.github.com>
2023-06-04 23:22:52 +0100

    attempted gene expression

--- gillespie/gene_expression.py
+++ gillespie/gene_expression.py
@@ -3,18 +3,35 @@ import matplotlib.pyplot as plt
 from gillespie import gillespie
 
 # Gene Expression:
-GE_stoch_subst = np.array([[]])
-GE_stoch_prods = np.array([[]])
-GE_rates = np.array([])
-GE_X0 = np.array([])
-GE_t_max = 10000
+
+# TOGGLE SWITCH:
+
+GE_stoch_subst = np.array([[1, 0, 0, 0, 0, 0],
+                           [0, 1, 0, 0, 0, 0],
+                           [0, 0, 1, 0, 0, 0],
+                           [0, 0, 0, 1, 0, 0],
+                           [0, 0, 1, 0, 0, 0],
+                           [0, 0, 0, 1, 0, 0],
+                           [0, 0, 0, 0, 1, 0],
+                           [0, 0, 0, 0, 0, 1]])
+GE_stoch_prods = np.array([[1, 0, 1, 0, 0, 0],
+                           [0, 1, 0, 1, 0, 0],
+                           [0, 0, 1, 0, 1, 0],
+                           [0, 0, 0, 1, 0, 1],
+                           [0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0]])
+GE_rates = np.array([0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01])
+GE_X0 = np.array([100, 100, 1000, 10, 100, 100])
+GE_t_max = 1000
 
 elapsed_time, X, t = gillespie(GE_X0, GE_rates,
                                GE_stoch_subst,
                                GE_stoch_prods,
                                GE_t_max)
 
-selected = None
+selected = [2, 3]
 
 print(
     f"Elapsed time: {elapsed_time}ms"
@@ -23,9 +40,9 @@ print(
 fig, ax = plt.subplots()
 if selected is None:
     for i in range(len(GE_X0)):
-        ax.plot(t, X[:, i], '-*', label='X' + str(i))
+        ax.plot(t, X[:, i], label='X' + str(i))
 else:
     for i in selected:
-        ax.plot(t, X[:, i], '-*', label='X' + str(i))
+        ax.plot(t, X[:, i], label='X' + str(i))
 ax.legend(loc='best', shadow=True)
 plt.show()
